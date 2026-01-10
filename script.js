ba58d28bd0a5f83f84adfeea840773a3c6feda2c
Jason Chen <jasonchen81107@gmail.com>
2026-01-09 16:40:08 -0800

    Website files

---
+++ script.js
@@ -0,0 +1,15 @@
+const code_btn = document.getElementById("code-btn");
+const games_btn = document.getElementById("games-btn");
+const art_btn = document.getElementById("3dart-btn");
+
+code_btn.addEventListener("click", () => {
+    alert("code clicked");
+});
+
+games_btn.addEventListener("click", () => {
+    alert("game clicked");
+});
+
+art_btn.addEventListener("click", () => {
+    alert("art clicked");
+});
